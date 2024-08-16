b4558bb69b6ab3cc19386a55bacf803dacdc1844
Hossam Hatem <hosam.hatem222@gmail.com>
2024-08-16 20:08:35 +0300

    add web icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,6 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang='en'>
+            <link rel="icon" href="/assets/images/logo.svg" />
             <body>
                 <Provider>
                     <div className='main'>
